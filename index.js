5c557ba1d4ea89e52354e546752aa90b07561ef0
Sarah Rappaport <SarahRappaport@Sarahs-MacBook-Pro-2.local>
2019-05-21 20:48:29 -0700

    fixed deletion localStorage issue

--- index.js
+++ index.js
@@ -67,6 +67,41 @@ function appendToDo() {
 
 function deleteItem() {
   event.target.parentNode.remove();
+  var htmlstring = event.target.parentNode.innerText;
+  truncateString(htmlstring)
+  }
+
+  function truncateString(htmlstring){
+    htmlstring=htmlstring.substring(4)
+    var isindex = htmlstring.indexOf(' is');
+    htmlstring = htmlstring.substring(0, isindex)
+    console.log(htmlstring.length)
+    htmlstring = htmlstring.trim()
+    console.log(htmlstring)
+    console.log(htmlstring.length)
+//open localStorage
+  for (var i = 0; i < localStorage.length; i++) {
+  var key = localStorage.key(0);
+  item_arr = JSON.parse(key);
+  var priority = localStorage.getItem(key)
+  priority_arr = JSON.parse(priority);
+  }
+  console.log(item_arr)
+  console.log(priority_arr)
+  htmlisat = item_arr.indexOf(htmlstring)
+  console.log(`here's where I am`)
+  console.log(htmlisat)
+  console.log(item_arr[htmlisat])
+  //delete item in first array and second array pair key/value
+  item_arr.splice(htmlisat, 1);
+  priority_arr.splice(htmlisat, 1);
+  console.log(item_arr)
+  console.log(priority_arr)
+//push item_array and priority_array back into localStorage
+  localStorage.clear()
+localStorage.setItem(JSON.stringify(item_arr), JSON.stringify(priority_arr));
+console.log(localStorage)
+
   }
 
 function storeList (listItem, listPriority) {
